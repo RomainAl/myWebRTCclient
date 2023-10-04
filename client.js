4a79dc857fa927ce76af7ef83bb45699bda8c3e8
RomainAl <romaninal@gmail.com>
2023-10-04 17:34:37 +0200

    maman2 consolelog

--- client.js
+++ client.js
@@ -3,6 +3,7 @@ try {
   //socket = io.connect("https://maman-jk7dceleka-od.a.run.app");
   socket = io.connect("https://maman2-jk7dceleka-od.a.run.app");
   //socket = io.connect("https://192.168.10.2:1337");
+  console.log("maman2");
 } catch(err){
   alert(err);
 }
