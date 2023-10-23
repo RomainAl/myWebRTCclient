0e088eb879d66ae34e4a0966466a03564a70028a
RomainAl <romaninal@gmail.com>
2023-10-23 14:51:03 +0200

    cut maman

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 let socket;
 try {
   //socket = io.connect("https://maman-jk7dceleka-od.a.run.app");
-  socket = io.connect("https://maman2-jk7dceleka-od.a.run.app");
+  //socket = io.connect("https://maman2-jk7dceleka-od.a.run.app");
   socket = io.connect("https://mywrtc-ro5o23vkzq-od.a.run.app");
   //socket = io.connect("https://192.168.10.2:1337");
   console.log("lulu");
